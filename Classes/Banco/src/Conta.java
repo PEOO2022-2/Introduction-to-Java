f808743973bad06b306046debbde2da83dc52c5c
Adrilene <adrilenefonseca@gmail.com>
2022-09-01 22:18:41 -0300

    Resolução dos exercícios da aula 3

---
+++ Classes/Banco/src/Conta.java
@@ -0,0 +1,22 @@
+public class Conta {
+    String titular;
+    String numero;
+    double saldo;
+
+    void depositar(double valor) {
+        this.saldo += valor;
+        System.out.println("Depositado o valor de " + valor + " na conta de " + this.titular);
+        System.out.println("Novo saldo " + this.saldo);
+    }
+
+    void sacar(double valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            System.out.println("Retirado o valor de " + valor + " na conta de " + this.titular);
+            System.out.println("Novo saldo " + this.saldo);
+        } else {
+            System.out.println("Não é possível realizar o saque, pois o valor solicitado é maior que saldo disponível");
+            System.out.println("Saldo disponível: " + this.saldo);
+        }
+    }
+}
---
+++ Classes/Banco/src/Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+    public static void main(String[] args){
+        Conta conta1 = new Conta();
+        conta1.titular = "Adrilene";
+        conta1.numero = "1010-10";
+        conta1.saldo = 800.40;
+
+        Conta conta2 = new Conta();
+        conta2.titular = "Denis";
+        conta2.numero = "1212-12";
+        conta2.saldo = 400.50;
+
+        conta1.depositar(200);
+        conta2.depositar(100);
+
+        conta1.sacar(20);
+        conta2.sacar(400);
+
+    }
+}
